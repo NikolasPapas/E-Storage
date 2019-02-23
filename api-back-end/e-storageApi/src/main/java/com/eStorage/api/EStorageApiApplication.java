67daa3ee62878b643b1c933d6137dd2229236d60
Nikolaos Papazian <nikolaspapazian@gmail.com>
2019-02-23 19:49:54 +0200

    Initial back End

---
+++ api-back-end/e-storageApi/src/main/java/com/eStorage/api/EStorageApiApplication.java
@@ -0,0 +1,13 @@
+package com.eStorage.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStorageApiApplication.class, args);
+	}
+
+}
